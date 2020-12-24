993efda00115c794493f5f440a194140e572afa9
lxw <1274699621@qq.com>
2020-12-24 19:37:44 +0800

    项目发布的版本

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -62,25 +62,6 @@ export default {
       return this.$message.error('获取折线图数据失败!')
     }
     var result = _.merge(res.data, this.options)
-    // 指定图表的配置项和数据
-    // var option = {
-    //   title: {
-    //     text: 'ECharts 入门示例'
-    //   },
-    //   tooltip: {},
-    //   legend: {
-    //     data: ['销量']
-    //   },
-    //   xAxis: {
-    //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    //   },
-    //   yAxis: {},
-    //   series: [{
-    //     name: '销量',
-    //     type: 'bar',
-    //     data: [5, 20, 36, 10, 10, 20]
-    //   }]
-    // }
 
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(result)
--- src/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  devServer: {
-    host: '192.168.1.7',
-    port: '8080',
-    open: true
-  }
-}
